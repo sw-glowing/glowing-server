972e69ebf843c93ab00d7afee9c4063502e79655
gkim <kimkm0419@naver.com>
2023-06-30 06:29:59 +0900

    [FIX] Add cors settings

--- src/main/java/com/swhack/glowing/global/config/WebConfig.java
+++ src/main/java/com/swhack/glowing/global/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-      .allowedOrigins("http://3.36.163.148/:8080")
+      .allowedOrigins("http://3.36.163.148:8080")
       .allowedMethods(HttpMethod.GET.name());
   }
 }
