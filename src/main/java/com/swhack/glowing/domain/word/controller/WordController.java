77f6f951522f9384b1be801194e943d06a18ef0a
iamsoojung <crystar2402@naver.com>
2023-06-30 03:12:27 +0900

    #1 ✨ [FEAT] Create word list API

--- src/main/java/com/swhack/glowing/domain/word/controller/WordController.java
+++ src/main/java/com/swhack/glowing/domain/word/controller/WordController.java
@@ -1,10 +1,15 @@
 package com.swhack.glowing.domain.word.controller;
 
+import com.swhack.glowing.domain.word.entity.Word;
 import com.swhack.glowing.domain.word.service.WordService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,5 +18,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class WordController {
 
   private final WordService wordService;
+
+  @GetMapping
+  public List<Word> getAllWords() {
+    return wordService.getAllWords();
+  }
 
 }
--- src/main/java/com/swhack/glowing/domain/word/service/WordService.java
+++ src/main/java/com/swhack/glowing/domain/word/service/WordService.java
@@ -1,13 +1,26 @@
 package com.swhack.glowing.domain.word.service;
 
+import com.swhack.glowing.domain.word.entity.Word;
 import com.swhack.glowing.domain.word.repository.WordRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class WordService {
 
   private final WordRepository wordRepository;
+
+  public Word getWordById(Long id) {
+    Optional<Word> wordOptional = wordRepository.findById(id);
+    return wordOptional.orElse(null);
+  }
+
+  public List<Word> getAllWords() {
+    return wordRepository.findAll();
+  }
 
 }
